d7a6481016ac7dd5c865f0110377a4c574c0e0b3
Connor <connor@murphysites.com>
2023-01-12 12:32:51 -0500

    remake config

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,5 @@ import yaml from "@rollup/plugin-yaml";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.croxyproxy.net/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
   plugins: [{ enforce: "pre", ...mdx() }, yaml(), react()],
 });
