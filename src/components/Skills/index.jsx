4449aebb55aeb564672b0f62efda1ecdc3c80877
Connor <connor@murphysites.com>
2023-01-14 01:25:30 -0500

    clean up homepage/skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -9,72 +9,74 @@ export default function Skills(props) {
     "MySQL",
     "GitHub",
     "PHP",
-    "Illustrator",
-    "Photoshop",
-    "Premiere Pro",
     "Figma",
     "Sass",
     "Markdown",
     "JavaScript",
+    "Illustrator",
+    "Photoshop",
+    "Premiere Pro",
     "HTML5",
     "CSS3",
-    "Raspberry Pi",
   ];
 
   function handleImgError(e) {
+    let newSrc = "";
     const currentSrc = e.currentTarget.src;
-    if (currentSrc.match("-original")) {
-      e.currentTarget.src = currentSrc.replace("-original", "-plain");
+    if (currentSrc.match("-plain")) {
+      newSrc = currentSrc.replace("-plain", "-line");
+    } else if (currentSrc.match("-line")) {
+      newSrc = currentSrc.replace("-line", "-original");
     } else {
-      e.currentTarget.src = currentSrc.replace("-plain", "-line");
+      newSrc = currentSrc.replace("-original", "-plain");
     }
+    e.currentTarget.src = newSrc;
   }
   const iconElements = [
     ...icons.map((icon) => {
       const iconName = icon.replace(/\s+/g, "").toLowerCase();
       return (
-        <div className="icon">
-          {/* <i
-            key={nanoid()}
-            className={`devicon-${iconName}-plain colored`}
-          /> */}
-          <img
+        <div className="icon" key={nanoid()}>
+          <i className={`devicon-${iconName}-${props.variant}`} />
+          {/* <img
             onError={handleImgError}
-            src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${iconName}/${iconName}-original.svg`}
-          />
+            src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${iconName}/${iconName}-${props.variant}.svg`}
+          /> */}
           <p>{icon}</p>
         </div>
       );
     }),
-    <div className="icon">
-      <svg
-        role="img"
-        aria-label="MDX"
-        class="icon icon-mdx"
-        viewBox="0 0 138 57"
-        width="69"
-        height="28.5"
-      >
-        <title>MDX</title>
-        <g>
-          <rect
-            width="136.5"
-            height="55.5"
-            x=".75"
-            y=".75"
-            rx="4.5"
-            fill="#0969da"
-          ></rect>
-          <g fill="none" stroke="white" stroke-width="6">
-            <path d="M16.5 44V19L30.25 32.75l14-14v25"></path>
-            <path d="M70.5 40V10.75"></path>
-            <path d="M57 27.25L70.5 40.75l13.5-13.5"></path>
-            <path d="M122.5 41.24L93.25 12M93.5 41.25L122.75 12"></path>
-          </g>
-        </g>
-      </svg>
-      <p>MDX</p>
-    </div>,
+    // <div className="icon">
+    //   <svg
+    //     role="img"
+    //     aria-label="MDX"
+    //     class="icon icon-mdx"
+    //     viewBox="0 0 138 57"
+    //     width="69"
+    //     height="28.5"
+    //   >
+    //     <title>MDX</title>
+    //     <g>
+    //       <rect
+    //         width="136.5"
+    //         height="55.5"
+    //         x=".75"
+    //         y=".75"
+    //         rx="4.5"
+    //         fill="#ffffff"
+    //       ></rect>
+    //       <mask>
+    //         <g fill="none" stroke-width="6">
+    //           <path d="M16.5 44V19L30.25 32.75l14-14v25"></path>
+    //           <path d="M70.5 40V10.75"></path>
+    //           <path d="M57 27.25L70.5 40.75l13.5-13.5"></path>
+    //           <path d="M122.5 41.24L93.25 12M93.5 41.25L122.75 12"></path>
+    //         </g>
+    //       </mask>
+    //     </g>
+    //   </svg>
+    //   <p>MDX</p>
+    // </div>,
   ];
   return <div className="iconholder">{iconElements}</div>;
 }
