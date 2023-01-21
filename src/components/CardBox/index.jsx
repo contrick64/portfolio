2e0eb864c0847f5fe79dd6294fa5be9e3f762488
Connor <connor@murphysites.com>
2023-01-20 23:31:50 -0500

    add keys to cards

--- src/components/CardBox/index.jsx
+++ src/components/CardBox/index.jsx
@@ -5,9 +5,10 @@ export default function CardBox(props) {
   return (
     <Container>
       <Row className="cardbox">
-        {props.children.map((child) => {
+        {props.children.map((child, index) => {
           return (
             <Col
+              key={index}
               xs={child.props.colSize ? child.props.colSize : 6}
               className="mb-3"
             >
